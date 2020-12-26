01afcbcd6ee1597db2fe7fbd4a51381541eb17d2
Will Bainton <baintonw@gmail.com>
2020-12-26 15:03:22 -0500

    solution to part one for day two

--- day-2/day-2-code.js
+++ day-2/day-2-code.js
@@ -16,14 +16,58 @@ function splitSecond(arr) {
 }
 
 function checkAllPaswords(arr) {
+    let validPasswords = 0;
     for(let i = 0; i < arr.length; i++) {
         let line = arr[i]
         let min = arr[i][0]
         let max = arr[i][1]
+        let char = arr[i][2]
+        char = char.replace(':','')
         let password = arr[i][3]
-        console.log(password)
+        console.log(min, max, char, password)
+        // console.log(char.replace(':',''))
+        if (hasAtLeast(min, char, password) && hasAtMost(max, char, password)) {
+            console.log('VALID')
+            console.log(min, max, char, password)
+            validPasswords++
+
+        } else {
+            // console.log('FALSE')
+            console.log(min, max, char, password)
+
+        }
     }
+    console.log(validPasswords)
 }
+
+function hasAtLeast(amount, char, string) {
+    let count = 0
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === char) {
+            count++
+        }
+    }
+    if (count >= amount) {
+        return true
+    }
+    return false
+}
+
+function hasAtMost(amount, char,  string) {
+    let count = 0
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === char) {
+            count++
+        }
+    }
+    if (count > amount) {
+        return false
+    }
+    return true
+}
+
+// console.log(!!hasAtLeast(1, 'a', 'apple'))
+// hasAtMost(4, 'a', 'aaaple')
 
 //Take the input and convert it to a string
 let text = fs.readFileSync("./input.txt")
@@ -39,4 +83,5 @@ const cleanedArr = []
 splitArr1.forEach(el => cleanedArr.push([el[0], ...el[1].split(' ')]))
 
 // console.log(cleanedArr)
+// checkAllPaswords(cleanedArr)
 checkAllPaswords(cleanedArr)
